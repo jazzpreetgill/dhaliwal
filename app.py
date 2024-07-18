2ea66c11e6cb9c7886667d5f8b6be82b2d72ed94
jazzpreetgill <140133292+jazzpreetgill@users.noreply.github.com>
2024-07-17 23:13:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,37 +17,25 @@ def predict(model, input_df):
     #prediction = int(prediction.iloc[0]['prediction_label'])
     return predictions
 
-def run():
 
+age = st.number_input('Age', min_value=1, max_value=100, value=25)
+sex = st.selectbox('Sex', ['male', 'female'])
+bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
+children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
+if st.checkbox('Smoker'):
+    smoker = 'yes'
+else:
+    smoker = 'no'
+region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
 
-    add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
-    ("Online", "Batch"))
+output=""
 
+input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
+input_df = pd.DataFrame([input_dict])
 
+if st.button("Predict"):
+    output = predict(model=model, input_df=input_df)
+    output = '$' + str(output)
 
-    age = st.number_input('Age', min_value=1, max_value=100, value=25)
-    sex = st.selectbox('Sex', ['male', 'female'])
-    bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-    children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
-    if st.checkbox('Smoker'):
-        smoker = 'yes'
-    else:
-        smoker = 'no'
-    region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
+st.success('The output is {}'.format(output))
 
-    output=""
-
-    input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
-    input_df = pd.DataFrame([input_dict])
-
-    if st.button("Predict"):
-        output = predict(model=model, input_df=input_df)
-        output = '$' + str(output)
-
-    st.success('The output is {}'.format(output))
-
-   
-
-if __name__ == '__main__':
-    run()
